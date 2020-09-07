a41be88cddd8e3abe1a3b70a47964cc405d2619c
Kenny Lajara <kennylajara@gmail.com>
2020-09-07 08:34:50 -0400

    Pugstagram design

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+    let thisYear = new Date().getFullYear();
+</script>
+
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+</style>
+
+<div class="Footer">
+    <div class="Fotter-container">
+        <div class="Footer-copy">&copy; {thisYear} Pugstagram</div>
+    </div>
+</div>
